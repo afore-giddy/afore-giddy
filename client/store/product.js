56948286434512ff07c1e812cd79f6c4d6c6f953
joecosta117 <joecosta117@gmail.com>
2018-10-30 16:15:07 -0400

    Product: created the product reducer and tests for it

---
+++ client/store/product.js
@@ -0,0 +1,41 @@
+import axios from 'axios'
+
+//Action types
+const GET_ALL_PRODUCTS = 'GET_ALL_PRODUCTS'
+const GET_SINGLE_PRODUCT = 'GET_SINGLE_PRODUCT'
+
+//Initial State
+const initialState = {
+  allProducts: [],
+  selectedProduct: { reviews: [] }
+}
+
+//Action Creators
+export const getAllProducts = allProducts => ({type: GET_ALL_PRODUCTS, allProducts})
+export const getSingleProduct = selectedProduct => ({type: GET_SINGLE_PRODUCT, selectedProduct})
+
+//Thunk
+export const fetchAllProducts = () => async dispatch => {
+  let res = await axios.get('/api/products')
+  let products = res.data
+  dispatch(getAllProducts(products))
+}
+
+export const fetchSingleProduct = (productId) => async dispatch => {
+  let res = await axios.get(`/api/products/${productId}`)
+  let product = res.data
+  dispatch(getSingleProduct(product))
+}
+
+//Reducer
+export const productsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_ALL_PRODUCTS:
+      console.log('hittinggggg')
+      return {...state, allProducts: action.allProducts}
+    case GET_SINGLE_PRODUCT:
+      return {...state, selectedProduct: action.selectedProduct}
+    default:
+      return state
+  }
+}
