5b5f15490bdf8ae6c454fb63ba503506fea1f66a
peter giampaoli <petergiampaoli@gmail.com>
2018-10-30 12:07:54 -0400

    added seed data

--- script/seed.js
+++ script/seed.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const db = require('../server/db')
-const {User} = require('../server/db/models')
+const {User, Product} = require('../server/db/models')
 
 async function seed() {
   await db.sync({force: true})
@@ -10,6 +10,198 @@ async function seed() {
   const users = await Promise.all([
     User.create({email: 'cody@email.com', password: '123'}),
     User.create({email: 'murphy@email.com', password: '123'})
+  ])
+
+  const collection = await Promise.all([
+    Collection.bulkCreate(
+      {
+        id: 1,
+        name: 'Lambo',
+        description: 'Nulla facilisi.'
+      },
+      {
+        id: 2,
+        name: 'Lotus',
+        description: 'Aenean fermentum.'
+      },
+      {
+        id: 3,
+        name: 'Bugoti',
+        description:
+          'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst.'
+      },
+      {
+        id: 4,
+        name: 'Masserati',
+        description:
+          'Praesent lectus. Vestibulum quam sapien, varius ut, blandit non, interdum in, ante.'
+      }
+    )
+  ])
+
+  const car = await Promise.all([
+    Product.bulkCreate(
+      {
+        id: 1,
+        make: 'Grand Caravan',
+        price: 78356.15,
+        imageArray: [{}],
+        onSale: true,
+        isFeatured: false,
+        description:
+          'Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.\n\nCras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+        collectionId: 2,
+        maxSpeed: 152,
+        engineType: 'V12',
+        transmission: 'Automatic',
+        acceleration: 3.3,
+        colors: 'Red'
+      },
+      {
+        id: 2,
+        make: 'Insight',
+        price: 80642.28,
+        imageArray: [{}],
+        onSale: true,
+        isFeatured: false,
+        description:
+          'Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.\n\nPellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.\n\nCum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+        collectionId: 2,
+        maxSpeed: 278,
+        engineType: 'V8',
+        transmission: 'Standard',
+        acceleration: 4.7,
+        colors: 'Silver'
+      },
+      {
+        id: 3,
+        make: 'Rio',
+        price: 88072.53,
+        imageArray: [{}],
+        onSale: false,
+        isFeatured: true,
+        description:
+          'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.\n\nIn hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.\n\nAliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.',
+        collectionId: 1,
+        maxSpeed: 207,
+        engineType: 'V12',
+        transmission: 'Automatic',
+        acceleration: 3.2,
+        colors: 'Red'
+      },
+      {
+        id: 4,
+        make: 'Bonneville',
+        price: 71797.87,
+        imageArray: [{}],
+        onSale: false,
+        isFeatured: false,
+        description:
+          'Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.\n\nIn quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.\n\nMaecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.\n\nMaecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.',
+        collectionId: 2,
+        maxSpeed: 191,
+        engineType: 'V8',
+        transmission: 'Standard',
+        acceleration: 3.1,
+        colors: 'Red'
+      },
+      {
+        id: 5,
+        make: 'Grand Caravan',
+        price: 79480.01,
+        imageArray: [{}],
+        onSale: true,
+        isFeatured: false,
+        description:
+          'In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.\n\nMaecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.\n\nMaecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.',
+        collectionId: 2,
+        maxSpeed: 231,
+        engineType: 'V8',
+        transmission: 'Standard',
+        acceleration: 5.0,
+        colors: 'White'
+      },
+      {
+        id: 6,
+        make: 'C-Class',
+        price: 80764.33,
+        imageArray: [{}],
+        onSale: false,
+        isFeatured: false,
+        description:
+          'Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.\n\nFusce consequat. Nulla nisl. Nunc nisl.',
+        collectionId: 1,
+        maxSpeed: 123,
+        engineType: 'Electric',
+        transmission: 'Standard',
+        acceleration: 4.3,
+        colors: 'Silver'
+      },
+      {
+        id: 7,
+        make: 'Town & Country',
+        price: 78233.43,
+        imageArray: [{}],
+        onSale: true,
+        isFeatured: true,
+        description:
+          'Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.\n\nIn congue. Etiam justo. Etiam pretium iaculis justo.',
+        collectionId: 1,
+        maxSpeed: 101,
+        engineType: 'Electric',
+        transmission: 'Standard',
+        acceleration: 4.9,
+        colors: 'Black'
+      },
+      {
+        id: 8,
+        make: 'Elantra',
+        price: 96057.71,
+        imageArray: [{}],
+        onSale: false,
+        isFeatured: false,
+        description:
+          'Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.\n\nAenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.\n\nCurabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.',
+        collectionId: 2,
+        maxSpeed: 109,
+        engineType: 'V12',
+        transmission: 'Automatic',
+        acceleration: 2.1,
+        colors: 'White'
+      },
+      {
+        id: 9,
+        make: 'Excel',
+        price: 97148.1,
+        imageArray: [{}],
+        onSale: true,
+        isFeatured: false,
+        description:
+          'Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.\n\nSed ante. Vivamus tortor. Duis mattis egestas metus.\n\nAenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.',
+        collectionId: 2,
+        maxSpeed: 169,
+        engineType: 'V8',
+        transmission: 'Standard',
+        acceleration: 4.6,
+        colors: 'Green'
+      },
+      {
+        id: 10,
+        make: 'Passat',
+        price: 98163.93,
+        imageArray: [{}],
+        onSale: false,
+        isFeatured: true,
+        description:
+          'Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.\n\nQuisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.',
+        collectionId: 1,
+        maxSpeed: 217,
+        engineType: 'V8',
+        transmission: 'Standard',
+        acceleration: 3.4,
+        colors: 'Blue'
+      }
+    )
   ])
 
   console.log(`seeded ${users.length} users`)
