67cabecd32d2cf0bdc1e023bd40ceb1e5b346152
Tyler Swartz <tmswartz12@gmail.com>
2018-10-30 12:58:35 -0400

    added associations and review model

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -3,19 +3,15 @@ const Product = require('./product')
 const Review = require('./review')
 const Collection = require('./collection')
 
-/**
- * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost, we might say:
- *
- *    BlogPost.belongsTo(User)
- */
+Review.belongsTo(Product)
+Review.belongsTo(User)
 
-/**
- * We'll export all of our models here, so that any time a module needs a model,
- * we can just require it from 'db/models'
- * for example, we can say: const {User} = require('../db/models')
- * instead of: const User = require('../db/models/user')
- */
+User.hasMany(Review)
+Product.hasMany(Review)
+
+Product.belongsTo(Collection)
+Collection.hasMany(Product)
+
 module.exports = {
   User,
   Product,
