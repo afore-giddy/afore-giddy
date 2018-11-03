61755dfc35859c5a4a56e1feb56eda423389cf69
joecosta117 <joecosta117@gmail.com>
2018-11-03 14:35:04 -0400

    added admin requirements for admin routes

--- server/api/admin_routes/admin_users.js
+++ server/api/admin_routes/admin_users.js
@@ -2,9 +2,27 @@ const router = require('express').Router()
 const {User} = require('../../db/models')
 module.exports = router
 
+//admin middleware
+const adminGate = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next()
+  } else {
+    res.sendStatus(401)
+  }
+}
+
+const idMatchGate = (req, res, next) => {
+  if (req.user && req.user.id === req.params.userId) {
+    next()
+  } else {
+    res.sendStatus(401)
+  }
+}
+
 //admin routes for access to users
+
 //get all users
-router.get('/users', async (req, res, next) => {
+router.get('/users', adminGate, idMatchGate, async (req, res, next) => {
   try {
     const users = await User.findAll()
     res.send(users)
@@ -14,7 +32,7 @@ router.get('/users', async (req, res, next) => {
 })
 
 //get user by id
-router.get('/users/:id', async (req, res, next) => {
+router.get('/users/:id', adminGate, idMatchGate, async (req, res, next) => {
   try {
     const id = req.params.id
     const user = await User.findAll({
@@ -38,7 +56,7 @@ router.get('/users/:id', async (req, res, next) => {
 })
 
 //delete user
-router.delete('/users/:id', async (req, res, next) => {
+router.delete('/users/:id', adminGate, idMatchGate, async (req, res, next) => {
   try {
     const id = req.params.id
     const deleted = await User.destroy({
@@ -51,17 +69,8 @@ router.delete('/users/:id', async (req, res, next) => {
   }
 })
 
-//create new user
-router.post('/users', async (req, res, next) => {
-  try {
-    res.send(await User.create(req.body))
-  } catch (err) {
-    next(err)
-  }
-})
-
 //update user info
-router.put('/users/:id', async (req, res, next) => {
+router.put('/users/:id', adminGate, idMatchGate, async (req, res, next) => {
   try {
     const updated = await User.findById(req.params.id)
     await updated.update(req.body)
