a6cd10bc40d6ad1d0b236d720ee553e722ee702b
peter giampaoli <petergiampaoli@gmail.com>
2018-11-06 14:38:15 -0500

    styling

--- server/db/models/user.spec.js
+++ server/db/models/user.spec.js
@@ -3,6 +3,7 @@
 const {expect} = require('chai')
 const db = require('../index')
 const User = db.model('user')
+const Product = db.model('product')
 
 describe('User model', () => {
   beforeEach(() => {
@@ -33,6 +34,44 @@ describe('User model', () => {
 
       it('returns false if the password is incorrect', () => {
         expect(cody.correctPassword('bonez')).to.be.equal(false)
+      })
+    }) // end describe('correctPassword')
+  }) // end describe('instanceMethods')
+}) // end describe('User model')
+
+describe('Product Model', () => {
+  beforeEach(() => {
+    return db.sync({force: true})
+  })
+
+  describe('instanceMethods', () => {
+    describe('correctMake', () => {
+      let car
+
+      beforeEach(async () => {
+        car = await Product.create({
+          make: 'Batmobile',
+          price: 1222424,
+          imageArray: [
+            'https://qph.fs.quoracdn.net/main-qimg-dcbc1b57b956a7647406c0bc65086c2f'
+          ],
+          onSale: false,
+          isFeatured: true,
+          description: '65478 Beilfuss Center',
+          maxSpeed: 100,
+          engineType: 'V8',
+          transmission: 'Standard',
+          acceleration: 3,
+          colors: 'White'
+        })
+      })
+
+      it('returns true if the password is correct', () => {
+        expect(car.correctMake('Batmobile')).to.be.equal(true)
+      })
+
+      it('returns false if the password is incorrect', () => {
+        expect(car.correctMake('NotBatMobile')).to.be.equal(false)
       })
     }) // end describe('correctPassword')
   }) // end describe('instanceMethods')
